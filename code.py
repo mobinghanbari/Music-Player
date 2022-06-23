e7579351b017c7789f4c987d2e96988a02fa2ce4
mobingh <mobinghanbari111@gmail.com>
2022-06-23 13:29:26 +0430

    The preparatory and elementary part of the program was created

--- code.py
+++ code.py
@@ -3,6 +3,25 @@ from pygame import mixer
 from tkinter import *
 import os
 
+#Preparatory and elementary part of the program
+root = Tk()
+root.title('Music Player')
+
+mixer.init()
+songstatus = StringVar()
+songstatus.set('Choosing')
+
+#Playlist section
+playlist = Listbox(root, selectmode=SINGLE)
+playlist.grid(columnspan=5)
+
+os.chdir(r'C:\Users\mobin\Music')
+songs = os.listdir()
+
+for song in songs:
+    playlist.insert(END, song)
+
+
 #The main functional part of the program
 def playsong():
     current_song = playlist.get(ACTIVE)
